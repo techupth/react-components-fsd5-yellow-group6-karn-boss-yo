f2ef4683604f2e99665577803234b17fa2d5f5fb
BOSS <bigboss1796@hotmail.com>
2023-12-18 11:06:13 +0700

    feat: Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import "./App.css";
 
-function App() {
+function Header() {
   return (
-    <div>
-      {/*
-        Todo: 1 ให้ Declare ตัว Component ย่อย 
-       เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <header> 
-      */}
-      <header>
+    <header>
         <nav>
           <div className="logo">🚀 XYZ CORP</div>
           <ul className="nav-list">
@@ -26,12 +21,12 @@ function App() {
           </ul>
         </nav>
       </header>
+  )
+}
 
-      {/*
-        Todo 2: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <main> 
-      */}
-      <main>
+function Main() {
+  return (
+    <main>
         <div className="company-detail-left">
           <h1>Welcome to XYZ Corp</h1>
           <p>Your Trusted Partner in Excellence</p>
@@ -60,12 +55,18 @@ function App() {
           <button className="learn-button">Learn More</button>
         </div>
       </main>
+  )
+}
 
-      {/*
-        Todo 3: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <section className="product-section"> 
-      */}
-      <section className="product-section">
+function AddToCartButton() {
+  return (
+    <button className="add-to-cart-button-secondary">Add to Cart</button>
+  )
+}
+
+function Section() {
+  return (
+    <section className="product-section">
         <div className="product-section-container">
           <h2 className="product-featured-heading">Featured Products</h2>
           <div className="product-card-list">
@@ -85,7 +86,7 @@ function App() {
                 ให้เปลี่ยน Style ของปุ่ม “Add to Card” แก้ไข className ใน Component `Button` ที่สร้างขึ้นมา
                 จาก “add-to-cart-button” เป็น “add-to-cart-button-secondary”
               */}
-              <button className="add-to-cart-button">Add to Cart</button>
+              <AddToCartButton />
             </div>
             <div className="product-card">
               <img
@@ -95,7 +96,7 @@ function App() {
               <h3>Classic Leather Watch</h3>
               <p className="product-price">4,000 Baht</p>
               {/* Todo 5: (จุดที่ 2) */}
-              <button className="add-to-cart-button">Add to Cart</button>
+              <AddToCartButton />
             </div>
             <div className="product-card">
               <img
@@ -105,19 +106,50 @@ function App() {
               <h3>Organic Green Tea</h3>
               <p className="product-price">79.99 Baht</p>
               {/* Todo 5: (จุดที่ 3) */}
-              <button className="add-to-cart-button">Add to Cart</button>
+              <AddToCartButton />
             </div>
           </div>
         </div>
       </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+        <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
+      </footer>
+  )
+}
+
+
+
+function App() {
+  return (
+    <div>
+      {/*
+        Todo: 1 ให้ Declare ตัว Component ย่อย 
+       เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <header> 
+      */}
+      <Header />
+
+      {/*
+        Todo 2: ให้ Declare ตัว Component ย่อย
+        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <main> 
+      */}
+      <Main />
+
+      {/*
+        Todo 3: ให้ Declare ตัว Component ย่อย
+        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <section className="product-section"> 
+      */}
+      <Section />
 
       {/*
         Todo 4: ให้ Declare ตัว Component ย่อย
         เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <footer> 
       */}
-      <footer>
-        <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
